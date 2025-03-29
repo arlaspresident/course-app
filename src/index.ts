fc6d9196a12ed8eeb948100d2081a7136d8fdc13
arlaspresident <jankovic4901@gmail.com>
2025-03-29 13:08:52 +0100

    lagt till funktionen för att lägga till en ny kurs

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,15 @@ interface CourseInfo {
     progressiom: 'A' | 'B' | 'C';
     syllabus: string;
 }
+
+let courses: CourseInfo[] = JSON.parse(localStorage.getItem('courses') || '[]');
+
+function addCourse(course: CourseInfo): void {
+    if (course.som(c => c.code === course.code)){
+        alert("kurskod måste vara unik");
+        return;
+    }
+    courses.push(course);
+    localStorage.setItem('courses', JSON.stringify(courses));
+    renderCourses();
+}
